efcaa19a5ca88e3ac43c8926cd25cc813fb08000
Fabien MARCHAND <ghost.fm65@gmail.com>
2021-10-18 13:41:47 +0200

    test toujours

--- projet_puis4.c
+++ projet_puis4.c
@@ -1,2 +1,20 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
+int tab[6][7];
+int player;
+
+int main(void){
+
+  printf(tab);
+  // if(cond){
+  // ;
+  // }
+  // else{
+  // ;
+  //}
+  
+
+  
+}
